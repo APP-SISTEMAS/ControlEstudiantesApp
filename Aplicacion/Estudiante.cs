1e06bf4b7bbf76bc72e36752a11177a1d0cebd4a
Ariel Alvarado <arielalvarado15@protonmail.com>
2024-06-12 15:15:00 -0600

    construccion del DER

--- Aplicacion/Estudiante.cs
+++ Aplicacion/Estudiante.cs
@@ -8,15 +8,62 @@ namespace EstructurasDatos
 {
     class Estudiante
     {
+        public int id = 0;
         public string nombre = "";
-        public DateTime? fechaNac;
+        public string apellido = "";
+        public DateTime? fechaNacimiento;
+        public string identificacion = "";
+        public char genero = 'M';
+        public bool activo = false;
+        public string telefono = "";
+        public string departamento = "";
+        public string municipio = "";
+        public string direccion = "";
+        public string correo = "";
+        public string tipoSangre = "";
+        public string tutor = "";
 
-        public void Registrar() { 
-        
+        public static Estudiante estudiante1 = new Estudiante();
+
+        public static void GestionEstudiante()
+        {
+            Console.WriteLine("Gestion de Estudiante");
+            Console.WriteLine("1)Registrar");
+            Console.WriteLine("2)Actualizar");
+            Console.WriteLine("3)Deshabilitar");
+            Console.WriteLine("Seleccione una opcion:");
+            int numeroOpcion = Convert.ToInt32(Console.ReadLine());
+            switch (numeroOpcion)
+            {
+                case 1:
+                    Console.WriteLine("Registrar Estudiante");
+                    estudiante1.Registrar();
+                    break;
+                case 2:
+                    Console.WriteLine("Actualizar Estudiante");
+                    estudiante1.Actualizar();
+                    break;
+                case 3:
+                    Console.WriteLine("Deshabilitar Estudiante");
+                    estudiante1.Deshabilitar();
+                    break;
+                default:
+                    Console.WriteLine("Opcion no valida");
+                    break;
+            }
         }
 
-        public void Deshabilitar() { 
-        
+
+        public void Registrar()
+        {
+        }
+        public void Actualizar()
+        {
+        }
+
+        public void Deshabilitar()
+        {
+
         }
     }
 }
--- Aplicacion/Program.cs
+++ Aplicacion/Program.cs
@@ -6,23 +6,40 @@ namespace EstructurasDatos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Aplicacion Control de Estudiantes!");
-
-            string nombre = "";
-            DateTime fechaNac;
-            int[] numero;
-
-
-
-            Estudiante estudiante1 = new Estudiante();
-            estudiante1.nombre = "Irvin";
-            estudiante1.fechaNac = null;
-            estudiante1.Registrar();
-            estudiante1.Deshabilitar();
-
+            bool continuar = true;
+            do
+            {
+                Console.WriteLine("Aplicacion Control de Centro Educativo");
+                Console.WriteLine("==================================");
+                Console.WriteLine("1)Gestion de Estudiante");
+                Console.WriteLine("2)Gestion de Clases");
+                Console.WriteLine("3)Gestion de Notas");
+                Console.WriteLine("4)Gestion de Reporteria");
+                Console.WriteLine("0)Salir");
+                Console.WriteLine("Seleccione una opcion:");
+                int numeroOpcion = Convert.ToInt32(Console.ReadLine());
+                switch (numeroOpcion)
+                {
+                    case 1:
+                        Estudiante.GestionEstudiante();
+                        break;
+                    case 2:
+                        //Console.WriteLine("Gestion de Clases");
+                        break;
+                    case 3:
+                        //Console.WriteLine("Gestion de Notas");
+                        break;
+                    case 4:
+                        //Console.WriteLine("Gestion de Reporteria");
+                        break;
+                    case 0:
+                        continuar = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida");
+                        break;
+                }
+            } while (continuar);
         }
-
-
-
     }
 }
