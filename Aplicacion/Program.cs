7132e43cde96387e7b2503877bf3e587cf80ca49
Ariel Alvarado <arielalvarado15@protonmail.com>
2024-06-24 10:32:04 -0600

    cambio de version a .net 6.0, antes en 5.0

--- Aplicacion/Program.cs
+++ Aplicacion/Program.cs
@@ -5,7 +5,7 @@ namespace EstructurasDatos
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             bool continuar = true;
             do
